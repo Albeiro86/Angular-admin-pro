f9569997fcf23390d55254e3f131bf880a7a4d81
Albeiro Vidal <albeiro.vidal.rojas@gmail.com>
2022-03-31 07:42:42 -0500

    diseño y rutas organizadas finalizado

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,40 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../auth/login/login.component';
-import { RegisterComponent } from '../auth/register/register.component';
-import { DashboardComponent } from '../pages/dashboard/dashboard.component';
-import { Grafica1Component } from '../pages/grafica1/grafica1.component';
-import { ProgressComponent } from '../pages/progress/progress.component';
-import { NopagefoundComponent } from '../pages/nopagefound/nopagefound.component';
-import { PagesComponent } from '../pages/pages/pages.component';
 
-const routes: Routes = [
+import { NopagefoundComponent } from '../404/nopagefound.component';
 
-  {path:'',
-   component: PagesComponent,
-   children: [
-    {path:'dashboard', component: DashboardComponent},
-    {path:'progress', component: ProgressComponent},
-    {path:'grafica1', component: Grafica1Component},
-    {path:'', redirectTo: '/dashboard', pathMatch:'full'},
+import { PagesRoutingModule } from '../pages/pages.routing';
+import { AuthRoutingModule } from '../auth/auth.routing';
 
-   ]
-  },
-  
-
-  {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent},
-  
-  
+const routes: Routes = [ 
+  // las rutas estan declaradas en los modulos correspondientes 
+  //pages.routing 
+  //aut.routing    
+  {path:'', redirectTo: '/dashboard', pathMatch:'full'},
   {path:'**', component: NopagefoundComponent},
-
 ];  
 
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
    
   ],
   exports:[
